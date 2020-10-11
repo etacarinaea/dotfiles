633eeafe83c2e2e4e0aed976cf99c443441840a5
Emily <etacarinaea@protonmail.com>
2020-10-11 23:28:47 +0200

    Add branch selection for install script

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -9,7 +9,17 @@ git clone --mirror $remote $gitdir || exit 1
 git --git-dir=$gitdir --work-tree=$HOME config \
   --local status.showUntrackedFiles no
 
-printf "Dotfiles have been succesfully initialised in %s\n" $gitdir
+while read -p "Select which branch to checkout (1: desktop, 2: t440s): "; do
+  if [[ $REPLY == 1 ]]; then
+    git --git-dir=$gitdir --work-tree=$HOME checkout desktop
+    break
+  elif [[ $REPLY == 2 ]]; then
+    git --git-dir=$gitdir --work-tree=$HOME checkout t440s
+    break
+  fi
+done
+
+printf "\nDotfiles have been succesfully initialised in %s\n" $gitdir
 printf "\n\
 To continue restart your shell so the new .bashrc is used. After that you can
 use \`config\` to interact with the dotfile repo, e.g. \`config status\` or
